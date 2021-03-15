05695bb7ffe79b1703925d0423d3df626401bcf4
Virij80@gmail.com <kbpf2005>
2021-03-15 19:05:41 +0300

    Добавьте файлы проекта.

---
+++ HomeWork14.cpp
@@ -0,0 +1,17 @@
+﻿
+#include <iostream>
+#include <string>   
+
+int main()
+{
+	std::string name;
+	name = "Aleksandr";
+
+	std::cout << name << "\n";
+	std::cout << name.length() << "\n";
+	std::cout << name[0] << "\n";
+	std::cout << name[name.length() -1];
+	return 0;
+
+}
+
